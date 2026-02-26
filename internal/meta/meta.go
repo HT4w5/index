ffc6c2a1f1afad5fe0bc63f02ec03749afe4e323
HT4w5 <ht4w5x@gmail.com>
2026-02-26 23:37:27 +0800

    feat(meta): improve version info

--- internal/meta/meta.go
+++ internal/meta/meta.go
@@ -16,8 +16,24 @@ var (
 
 var (
 	VersionShort string
+	VersionLong  string
 )
 
 func init() {
 	VersionShort = fmt.Sprintf("%s %s", Name, Version)
+	VersionLong = fmt.Sprintf(
+		"%s %s %s (%s %s)",
+		Name,
+		Version,
+		firstN(CommitHash, 7),
+		GoVersion,
+		Platform,
+	)
+}
+
+func firstN(s string, n int) string {
+	if len(s) > n {
+		return s[:n]
+	}
+	return s
 }
